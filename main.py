8774a466c81753c63ef41672efa44803e04028f8
gilneyjr <gilneyjnr@gmail.com>
2020-12-10 10:25:09 -0300

    GUI is working...

--- main.py
+++ main.py
@@ -1,87 +1,38 @@
 #!/usr/bin/python3
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_leaflet as dl
-import dash_table
-from random import uniform as rand
 from dash.dependencies import Input, Output, State
-import pandas as pd
 from flask import Flask, request, Response
+from map import getMarker
+from panels import createMainPanel
 
-# Local imports
-import dropdown
-import map
-
-df = pd.DataFrame(
-[
-    ["State", "Number of Solar Plants", "Installed Capacity (MW)", "Average MW Per Plant", "Generation (GWh)"],
-    ["California",289,4395,15.3,10826],
-    ["Arizona",48,1078,22.5,2550],
-    ["Nevada",11,238,21.6,557],
-    ["New Mexico",33,261,7.9,590],
-    ["Colorado",20,118,5.9,235],
-    ["Texas",12,187,15.6,354],
-    ["North Carolina",148,669,4.5,1162],
-    ["New York",13,53,4.1,84]
-]
-)
-
-server = Flask(__name__)    
+server = Flask(__name__)
 
 @server.route('/', methods=['POST'])
 def home():
     callback(request.json)
     return Response(status=200)
 
-app = dash.Dash(__name__, server=server, prevent_initial_callbacks=True)
-app.layout = html.Div([
-    dl.Map(
-        center=(-5.811967825768887, -34.20487439621176), 
-        zoom=7, 
-        children=[dl.TileLayer(), dl.LayerGroup(children=[],id="layer")],
-        id="map", 
-        style={
-            'width': '50%', 
-            'height': '100%', 
-            'margin': "0", 
-            "display": "block"
+app = dash.Dash(
+    __name__, 
+    server=server, 
+    prevent_initial_callbacks=True,
+    meta_tags=[
+        {'charset': 'utf-8'},
+        {
+          'name': 'viewport',
+          'content': 'width=device-width, initial-scale=1.0'
         }
-    ),
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        style_table={
-            'max-width':'50%',
-            'max-height': '100%',
-            'margin': "0", 
-            "display": "block"
-        }
-    ),
-    # Increment in time
-    dcc.Interval(
-        id='interval-component',
-        interval=1*1000, # in milliseconds
-        n_intervals=0
-    )
-], 
-style={
-    'width': '100vw', 
-    'height': '100vh',
-    'display': 'flex-container',
-    'justify-content': 'space-between',
-    'align-items': 'flex-start',
-})
+    ],
+    title='Ofertados',
+    update_title=None
+)
+app.layout = createMainPanel()
 
 @app.callback(
     Output("layer", "children"),
     Input("interval-component", "n_intervals"),
-    State("layer", "children")
-    )
-def map_click(n_intervals, marks):
-# def map_click(click_lat_lng):
-    print(type(marks))
+    State("layer", "children"))
+def map_update(n_intervals, marks):
     if marks == None:
         marks = []
     marks.append(getMarker())
--- map.py
+++ map.py
@@ -1,18 +1,12 @@
 import dash_leaflet as dl
 from random import uniform as rand
 
-def getMap(id='map-promocoes', center=(-5.811967825768887, -34.20487439621176), zoom=7):
+def getMap(id='map', center=(-5.811967825768887, -34.20487439621176), zoom=7):
 	return dl.Map(
         center=center, 
         zoom=zoom, 
         children=[dl.TileLayer(), dl.LayerGroup(children=[],id="layer")],
-        id=id, 
-        style={
-            'width': '50%', 
-            'height': '100%', 
-            'margin': "0", 
-            "display": "block"
-        }
+        id=id
     )
 
 def getMarker(position=None):
